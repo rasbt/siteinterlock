8d7e2a2c32f2d922ee8e2ab2c09b55a47e9111a1
rasbt <mail@sebastianraschka.com>
2016-08-18 16:09:19 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 # Sebastian Raschka 2016
+# Copyright (C) 2016 Michigan State University
 #
 # siteinterlock, a novel algorithm for protein-ligand
 # docking based on graph theory.
 #
 # Author: Sebastian Raschka <sebastianraschka.com>
+# Author email: raschkas@msu.edu
 #
-# License: {placeholder}
+# License: GPL v3
 
 from setuptools import setup, find_packages
 import siteinterlock
@@ -14,43 +16,52 @@ VERSION = siteinterlock.__version__
 
 setup(name='siteinterlock',
       version=VERSION,
-      description='{placeholder}',
+      description=('A novel approach to pose selection in'
+                   'protein-ligand docking based on graph theory.'),
       author='Sebastian Raschka',
       author_email='raschkas@msu.edu',
-      url='https://github.com/rasbt/siteinterlock',
+      url='https://github.com/psa-lab/siteinterlock',
       packages=find_packages(),
       package_data={'': ['LICENSE', 'README.md', 'requirements.txt']},
-      # package_data={'': ['LICENSE', 'README.md', 'requirements.txt']
-      # },
       include_package_data=True,
       license='{placeholder}',
       platforms='any',
       classifiers=[
-             # 'License :: OSI Approved :: BSD License',
-             'Operating System :: Microsoft :: Windows',
-             'Operating System :: POSIX',
-             'Operating System :: Unix',
-             'Operating System :: MacOS',
-             'Programming Language :: Python :: 3',
-             'Programming Language :: Python :: 3.3',
-             'Programming Language :: Python :: 3.4',
-             'Programming Language :: Python :: 3.5',
-             'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Operating System :: MacOS',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering',
       ],
       long_description="""
 
-{placeholder}
-
+A novel approach to pose selection in protein-ligand
+docking based on graph theory.
 
 Contact
 =============
 
-If you have any questions or comments about siteinterlock,
-please feel free to contact me via
-eMail: mail@sebastianraschka.com
+SiteInterlock is a Python package for selecting near-native protein-ligand
+docking poses based on the hypothesis that interfacial rigidification of
+both the protein and ligand prove to be important characteristics of the
+native binding mode and are sensitive to the spatial coupling of
+interactions and bond-rotational degrees of freedom in the interface.
 
-This project is hosted at https://github.com/rasbt/siteinterlock
+The `siteinterlock` package is being developed in the
+Protein Structure Analysis & Design Laboratory (http://www.kuhnlab.bmb.msu.edu)
+at Michigan State University.
 
-The documentation can be found at {placeholder}
+The source code is hosted at https://github.com/psa-lab/siteinterlock and
+you can contact the original author of this
+software package at raschkas@msu.edu. The email address for general questions
+about the siteinterlock package, please use the following address:
+kuhnlab@msu.edu
 
 """)
